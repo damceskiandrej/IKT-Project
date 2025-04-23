09cebb1d097552c7f70dd667bde12cae7cdf4424
Filip Petkovski <filip.petkovski.work@gmail.com>
2025-04-23 21:45:44 +0200

    Split the buttons

--- edu-quiz-frontend/front/src/components/CustomCard2.jsx
+++ edu-quiz-frontend/front/src/components/CustomCard2.jsx
@@ -7,7 +7,9 @@ function CustomCard2() {
                 <h4 className="card-title">Агентски базирани системи и нивна примена</h4>
                 <p className="card-text">Развој и симулација на автономни агенти во различни сложени средини.</p>
                 <div className="d-flex justify-content-center">
-                    <CustomButton btnText={"КВИЗ"} className="pe-2"/>
+                    <div className="me-2">
+                        <CustomButton btnText={"КВИЗ"}/>
+                    </div>
                     <CustomButton btnText={"СОВЕТ"}/>
                 </div>
             </div>
