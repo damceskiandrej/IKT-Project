b075be16f07d56bc193fd2281a1ead9b2621dfb9
Mario Jovanovski <mario.jovanovski@infinite.com.mk>
2025-04-28 02:07:55 +0200

    QuizQuestionsPage, ResultPage, CustomReviewCard, CustomAnswer changes + Answer validation added

--- edu-quiz-frontend/front/src/components/CustomReviewCard.jsx
+++ edu-quiz-frontend/front/src/components/CustomReviewCard.jsx
@@ -1,33 +1,37 @@
-function CustomReviewCard() {
-    return (
-      <div className="container p-3">
-        <div className="border rounded p-3">
-          <div className="d-flex align-items-start mb-3">
-            <span className="badge bg-dark me-2">1</span>
-            <h6 className="mb-0">
-              КОЕ ОД СЛЕДНИВЕ Е ОСНОВНА КАРАКТЕРИСТИКА НА АГЕНТИТЕ?
-            </h6>
-          </div>
-  
-          <div className="d-flex flex-column">
-            <div className="mb-2">A СТАТИЧНОСТ</div>
-  
-            <div
-              className="mb-2 p-2 rounded bg-success bg-opacity-25 d-flex justify-content-between align-items-center"
-              style={{ maxWidth: "300px" }}>
-              <span>B АВТОНОМНОСТ</span>
-              <span className="text-success">✔️</span>
-            </div>
-  
-            <div className="mb-2 text-decoration-line-through text-danger">
-              C ДИРЕКТНА КОНТРОЛА ОД КОРИСНИК
-            </div>
-  
-            <div className="mb-2">D НЕПРОМЕНЛИВОСТ</div>
-          </div>
+function CustomReviewCard({ question, selectedAnswer}) {
+  return (
+    <div className="container p-3">
+      <div className="border rounded p-3">
+        <div className="d-flex align-items-start mb-3">
+          <span className="badge bg-dark me-2">{question.id}</span>
+          <h6 className="mb-0">{question.questionText}</h6>
+        </div>
+
+        <div className="d-flex flex-column">
+          {question.answers.map((answer, index) => {
+            const isSelected = selectedAnswer === answer.answerText;
+            const isCorrect = answer.isCorrect;
+            const isIncorrect = isSelected && !isCorrect;
+
+            return (
+              <div
+                key={index}
+                className={`mb-2 p-2 rounded d-flex justify-content-between align-items-center ${isCorrect ? 'bg-success bg-opacity-25' : ''} ${isIncorrect ? 'text-danger' : ''}`}
+                style={{ maxWidth: "300px" }}
+              >
+                <span>{String.fromCharCode(65 + index)} {answer.answerText}</span>
+                {isCorrect && isSelected ? (
+                  <span className="text-success">✔️</span> 
+                ) : isIncorrect ? (
+                  <span className="text-danger">❌</span> 
+                ) : null}
+              </div>
+            );
+          })}
         </div>
       </div>
-    )
-  }
-  
-  export default CustomReviewCard
+    </div>
+  )
+}
+
+export default CustomReviewCard;
