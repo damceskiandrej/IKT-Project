afc75b64933b05ead22b5c330e55ce6f8770330d
Mario Jovanovski <mario.jovanovski@infinite.com.mk>
2025-05-05 12:23:28 +0200

    Post quiz implementation

--- edu-quiz-frontend/front/src/api/quizApi.js
+++ edu-quiz-frontend/front/src/api/quizApi.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 
 const URL = "http://localhost:5190/api/quiz"
 
@@ -18,12 +16,36 @@ export const getQuizById = async (id) => {
             throw new Error(`Failed to fetch quiz: ${response.statusText}`);
         }
         const data = await response.json();  
+        console.log("getQuiz", data)
         return data;  
     } catch (error) {
         console.error("Error fetching quiz:", error); 
         throw error;  
     }
 }
+
+export const postQuizResult = async (submission) => {
+    try {
+        const response = await fetch(`${URL}/SubmitQuizResult`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(submission),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to submit quiz: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log("Submission response:", data); // You can log or process the response as needed
+        return data;
+    } catch (error) {
+        console.error("Error submitting quiz:", error);
+        throw error;
+    }
+};
 
 
 
