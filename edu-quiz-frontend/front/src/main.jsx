4b830379f3028c642cb8f49e32ca44218eee5499
Filip Petkovski <filip.petkovski.work@gmail.com>
2025-05-10 19:49:08 +0200

    Add text translation

--- edu-quiz-frontend/front/src/main.jsx
+++ edu-quiz-frontend/front/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App.jsx'
+import React from 'react';
+import './i18n'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
