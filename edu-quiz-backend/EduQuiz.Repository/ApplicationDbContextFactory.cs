9df0cabef92a98bf22f4fdb390f7788d0358a050
vladimir028 <107487074+vladimir028@users.noreply.github.com>
2025-05-13 18:40:32 +0200

    comment mario's DbContextFactory

--- edu-quiz-backend/EduQuiz.Repository/ApplicationDbContextFactory.cs
+++ edu-quiz-backend/EduQuiz.Repository/ApplicationDbContextFactory.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using System.IO;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+//using Microsoft.Extensions.Configuration;
+//using System.IO;
 
-namespace EduQuiz.Repository
-{
-    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext CreateDbContext(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("C:/Users/Mario/Documents/IKT-Project/edu-quiz-backend/EduQuizWebApplication/appsettings.json") // or appsettings.Development.json
-                .Build();
+//namespace EduQuiz.Repository
+//{
+//    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+//    {
+//        public ApplicationDbContext CreateDbContext(string[] args)
+//        {
+//            var configuration = new ConfigurationBuilder()
+//                .SetBasePath(Directory.GetCurrentDirectory())
+//                .AddJsonFile("C:/Users/Mario/Documents/IKT-Project/edu-quiz-backend/EduQuizWebApplication/appsettings.json") // or appsettings.Development.json
+//                .Build();
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+//            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+//            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
 
-            return new ApplicationDbContext(optionsBuilder.Options);
-        }
-    }
-}
+//            return new ApplicationDbContext(optionsBuilder.Options);
+//        }
+//    }
+//}
